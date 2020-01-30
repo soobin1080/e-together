67f18b6f53e974fad165b887ae1263b2863cd2dd
안지연 <jennyahn94@naver.com>
2020-01-30 11:27:26 +0900

    200130_ahnzzi_morning

--- ssafy-frontend/src/components/ProductList.vue
+++ ssafy-frontend/src/components/ProductList.vue
@@ -1,11 +1,11 @@
 <template>
   <v-layout mt-5 wrap>
-    <v-flex v-for="i in products.length > limits ? limits : products.length" xs12 sm6 lg4 :key="i">
+    <v-flex  v-for="i in product.length > limits ? limits : product.length" xs12 sm6 lg4 :key="i">
       <Product
         class="ma-3"
-        :pro_name="products[i - 1].pro_name"
-        :price="products[i - 1].price"
-        :img="products[i - 1].img"
+        :pro_name="product[i - 1].pro_name"
+        :price="product[i - 1].price"
+        :img="product[i - 1].img"
       ></Product>
       <v-divider></v-divider>
     </v-flex>
@@ -26,74 +26,34 @@ export default {
   props: {
     limits: { type: Number, default: 454 },
     loadMore: { type: Boolean, default: false },
-    keyword: { type: String, default: "" }
+    product: {type: Object}
   },
   data() {
     return {
-      products: []
     };
   },
   components: {
     Product
   },
+ 
   created() {
     // console.log("create: productlist에서 뿌려줘!!!!:" + this.keyword);
-    this.getProductList();
   },
   mounted() {
     // console.log("mount: productlist에서 뿌려줘!!!!:" + this.keyword);
-    this.getProductList();
-    this.search();
+    this.all();
   },
   methods: {
-    getProductList() {
-      // this.$emit('search');
-      console.log("키워드는" + this.keyword);
-      if (this.keyword != "" && this.keyword.length > 0) {
-        this.search();
-      } else {
-        this.all();
-      }
-    },
     all() {
-      http
-        .get("/product")
-        .then(response => {
-          console.log(response.data);
-          this.products = response.data;
-          // console.log("product:"+this.products);
-          // console.log(this.products.length);
-        })
-        .catch(() => {
-          this.errored = true;
-          // alert("error!!");
-        })
-        .finally(() => (this.loading = false));
+      product=this.$attrs.product
     },
-    search() {
-      
-      console.log("리스트에서 찾아라" + this.keyword);
-      http
-        .get("/product/" + this.keyword)
-        .then(response => {
-          console.log(response.data);
-          this.products = response.data;
-        })
-        .catch(() => {
-          this.errored = true;
-          // alert("error!!");
-        })
-        .finally(() => (this.loading = false));
-    },
+    
     loadMoreProducts() {
       this.limits = this.limits + 4;
       if (this.limits >= this.products.length) {
         this.loadMore = false;
       }
     }
-  },
-  computed:{
-
   }
 };
 </script>
