4c90cace6e66726507ef01b7076a75d8ac8578c1
안지연 <jennyahn94@naver.com>
2020-02-10 19:50:23 +0900

    200210_ahnzzi_commit

--- ssafy-frontend/src/components/MyBudgetDetail.vue
+++ ssafy-frontend/src/components/MyBudgetDetail.vue
@@ -1,44 +1,224 @@
 <template>
- 
+  <div>
+    <table width="100%" style="font-size:15px">
+      <tr style="text-align:center;">
+        <th>제목</th>
+        <td v-html="computedBudgetDetail.budget_title" style="text-align:center"></td>
+
+        <th>인원</th>
+        <td style="text-align:center">{{detail.personnel}} 명</td>
+
+        <th>예산</th>
+        <td style="text-align:center">{{detail.budget}} 원</td>
+
+        <th>날짜</th>
+        <td style="text-align:center">{{dateParsing(detail.date)}}</td>
+
+        <th>적/부</th>
+        <td style="text-align:center">
+          {{detail.fitness}}
+          <div>
+            <v-btn class="ma-2" text icon color="blue lighten-2">
+              <v-icon>mdi-thumb-up</v-icon>
+            </v-btn>
+
+            <v-btn class="ma-2" text icon color="red lighten-2">
+              <v-icon>mdi-thumb-down</v-icon>
+            </v-btn>
+          </div>
+        </td>
+      </tr>
+    </table>
+
+    <table style="width:100%; margin:auto; text-align:center">
+      <col width="45%" />
+      <col width="15%" />
+      <col width="25%" />
+      <thead >
+        <tr >
+          <th  style="background-color: #fffeaa; color:#3d2300">항목</th>
+          <th  style="background-color: #fffeaa; color:#3d2300">수량</th>
+          <th  style="background-color: #fffeaa; color:#3d2300">가격</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="i in detail.budgetlist.length" :key="i" >
+          <td v-html="detail.budgetlist[i-1].pro_name"></td>
+
+          <td v-html="detail.budgetlist[i-1].quantity"></td>
+
+          <td class="pro_price">{{detail.budgetlist[i-1].price}} 원</td>
+        </tr>
+
+        <tr >
+          <th colspan="2">합계</th>
+         <td>{{this.total}} 원</td>
+        </tr>
+          <tr >
+          <th colspan="2">잔액</th>
+         <td>{{this.detail.budget-this.total}} 원</td>
+        </tr>
+      </tbody>
+    </table>
+    <br />
+    <div style="text-align:center">
+      <v-btn outlined color="success">pdf로 저장</v-btn>
+      <v-btn outlined color="error" @click.stop="dialog=true">후기 남기기</v-btn>
+    </div>
+
+
+     <!-- 이미지, 내용 등록 modal -->
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">후기 등록</v-card-title>
+
+        <v-col>
+          <v-text-field label="이미지를 등록해주세요" v-model="img"></v-text-field>
+          <v-text-field autocomplete="nope" label="내용을 입력해주세요" v-model="content"></v-text-field>
+        </v-col>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="writeReview(true)">저장</v-btn>
+          <v-btn color="green darken-1" text @click="writeReview(false)">취소</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "MyBudgetDetail",
-    props: {
-    date: { type: Number },
-    title: { type: String },
-    body: { type: String },
+import http from "../http-common";
+export default {
+  name: "MyBudgetDetail",
+  props: {
+    budgetDetail: {
+      type: Array
+    },
+    title :{ 
+      type : String
+    }
   },
 
-    data() {
-      return {
+  data() {
+    return {
+      // budgetDetail: [],
+      detail: {},
+      total: 0,
+      budgetlength: 0,
+      dialog : false,
+      content :"",
+      img:""
+    };
+  },
 
+  methods: {
+    getMyBudgetDetail(budgetDetail) {
+      let myEmail = localStorage.getItem("email");
+      console.log("!myEmail :" + myEmail);
+      let mytitle = budgetDetail.budget_title;
+      console.log("!mytitle :" + mytitle);
+      http
+        .get(
+          "/budget/" + myEmail + "/" + mytitle,
+          {
+            user_email: myEmail,
+            budget_title: mytitle
+          },
+          this.$store.getters.RequestHeader
+        )
+        .then(res => {
+          console.log("getMyBudgetDetail");
+          console.log(res)
+          this.detail = res.data;
+          console.log("데이터 : " + this.detail);
+          console.log("항목 : " + this.detail.budgetlist);
+          console.log("항목 : " + this.detail.budgetlist.length);
+          this.budgetlength=this.detail.budgetlist.length;
+          for (let index = 0; index < this.budgetlength; index++) {
+         this.total += Number(this.detail.budgetlist[index].price);          
+       }  
+      return this.total;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    dateParsing(beforeParsing) {
+      const t = beforeParsing.indexOf("T");
+      const afterParsing = beforeParsing.substring(0, t);
+      console.log(afterParsing);
+      return afterParsing;
+    },
+      total_sum() {  
+       console.log("budgetList 길이!:"+ this.budgetlength); 
+       for (let index = 0; index < this.detail.budgetlist; index++) {
+         this.total += detail.budgetlist[index].price;          
+       }  
+      return this.total;
+    },
+    writeReview(bool){
+     if (bool === true) {
+        if (this.content == "") {
+          alert("내용을 입력해주세요.");
+          return;
+        }       
+        http
+          .post("/review", {
+            user_email: localStorage.getItem('email'),
+            budget_title: this.title,
+            img: this.img,
+            content: this.content            
+          }, this.$store.getters.requestHeader)
+          .then(response => {
+            console.log(response)
+            // this.result = response.;
+          })
+          .catch(ex => {
+            console.warn("ERROR! :", ex);
+          });
+        this.$router.push("/review");
+      } else {        
+        this.dialog=false;
       }
-    },
-
-    methods: {
-      show_detail(num) {
-      
     }
+  },
+  mount() {},
 
+  computed: {
+    convertDate() {
+      console.log("convertDate");
+      let convertDate = new Date(this.date);
+      console.log(convertDate);
+      return this.date;
     },
-    mount() {
-
-    },
-
-    computed: {
-     convertDate() {
-       console.log("convertDate")
-       let convertDate = new Date(this.date)
-       console.log(convertDate)
-       return this.date
-     }
+    computedBudgetDetail: function() {
+      console.log('computedBudgetDetail')
+      console.log(this.budgetDetail)
+      this.getMyBudgetDetail(this.budgetDetail)
+      return this.budgetDetail
     }
-
+  },
+  created() {
+    this.getMyBudgetDetail(budgetDetail);
+    this.total_sum();
   }
+};
 </script>
 
 <style scoped>
-
+table {
+  width: 100%;
+  border-top: 1px solid dimgray;
+  border-collapse: collapse;
+}
+th,
+td {
+  border-bottom: 1px solid dimgray;
+  padding: 5px;
+}
+th {
+  color:dimgray;
+  background-color: lightgray;
+}
 </style>
--- ssafy-frontend/src/views/MyBudgetPage.vue
+++ ssafy-frontend/src/views/MyBudgetPage.vue
@@ -1,163 +1,130 @@
 <template>
   <div>
-    <!-- <input type="text" name="title" v-model="title" />
-    <input type="text" name="body" v-model="body" />
-    <v-btn @click="postMyBudgets(title, body)">add</v-btn>-->
     <v-img :src="getImgUrl('budgetlist.jpg')" aspect-ratio="5.5">
       <v-layout align-center justify-center row fill-height>
         <v-flex text-xs-center>
           <span class="text-shadow display-2 font-weight-light">
             <slot name="text" />
             <div
-        class="text-center text-white"
-        style="line-height:1.2em; font-size:2.5em;"
-        slot="text"
-        v-resize-text
-      >My Budget</div>
+              class="text-center text-white"
+              style="line-height:1.2em; font-size:2.5em;"
+              slot="text"
+              v-resize-text
+            >My Budget</div>
           </span>
         </v-flex>
-      </v-layout>       
+      </v-layout>
     </v-img>
-   
+
     <div class="main">
-      <v-card style="width:80%; height: 100%;" class="mx-auto my-5 flat">
-        <MyBudgetList 
+      <div style="width:60%; height: 100%;" class="mx-auto my-5 flat">
+        <MyBudgetList
           :allBudgets="allBudgets"
           :budgetPerPage="budgetPerPage"
-          :pages="pages"></MyBudgetList>
-      </v-card>
+          :pages="pages"
+          v-on:showdetail="showdetail"
+        ></MyBudgetList>
+      </div>
+     
 
       <div class="text-center">
-        <v-pagination 
-          v-model="pages" 
-          :length="pagingLength" 
-          total-visible="9" 
-          ></v-pagination>
+        <v-pagination v-model="pages" :length="pagingLength" total-visible="9"></v-pagination>
       </div>
     </div>
-  </div>
+    <div class="mx-auto my-5 flat" style="width:60%" v-if="showflag==true">
+     <MyBudgetDetail
+      :budgetDetail="budgetDetail" :title="title"></MyBudgetDetail>
+    </div>
+
+  </div>  
 </template>
 
 <script>
 import FirebaseService from "@/services/FirebaseService";
 import ImgBanner from "../components/ImgBanner";
 import MyBudgetList from "../components/MyBudgetList";
+import MyBudgetDetail from "../components/MyBudgetDetail";
 import ResizeText from "vue-resize-text";
 import http from "../http-common";
 export default {
-  name: "MyListPage",
+  name: "MyBudgetPage",
 
   components: {
     ImgBanner,
-    MyBudgetList
+    MyBudgetList,
+    MyBudgetDetail
   },
   directives: {
     ResizeText
   },
   data: () => ({
-    title: "",
-    body: "",
     pages: 1,
     allBudgets: [],
     pagingList: [],
     pagingLength: 0,
     allLength: 0,
     budgetPerPage: 5,
-    items: [
-      {
-        icon: "folder",
-        iconClass: "grey lighten-1 white--text",
-        title: "Photos",
-        subtitle: "Jan 9, 2014"
-      },
-      {
-        icon: "folder",
-        iconClass: "grey lighten-1 white--text",
-        title: "Recipes",
-        subtitle: "Jan 17, 2014"
-      },
-      {
-        icon: "folder",
-        iconClass: "grey lighten-1 white--text",
-        title: "Work",
-        subtitle: "Jan 28, 2014"
-      }
-    ]
+    budgetDetail: [],
+    showflag : false
   }),
   methods: {
     getImgUrl(img) {
       return require("../assets/" + img);
     },
-    // postMyBudgets(title, body) {
-    //   title = this.title;
-    //   body = this.body;
-    //   FirebaseService.postMyBudgets(title, body);
-    //   this.getMyBudgets();
-    // },
-    // async getMyBudgets() {
-    //   console.log("active");
-    //   this.pagingList = await FirebaseService.getMyBudgets();
-    //   this.pagingLength = parseInt(this.pagingList / 5) + 1;
-    //   return this.pagingList;
-    // },
-    getMyBudgets() {
-      let myEmail = localStorage.getItem('email')
-      http
-        .get(`/budget/${myEmail}`, {
-          user_email : myEmail
-        }, this.$store.getters.RequestHeader)
-        .then(res => {
-          console.log('getMyBudget')
-          this.allBudgets = res.data
-          this.allLength = res.data.length
-          this.pagingList = this.allBudgets
 
-          this.allLength = this.pagingList.length
+    getMyBudgets() {
+      let myEmail = localStorage.getItem("email");
+      http
+        .get(
+          "/budget/"+myEmail,
+          {
+            user_email: myEmail
+          },
+          this.$store.getters.RequestHeader
+        )
+        .then(res => {
+          console.log("getMyBudget");
+          this.allBudgets = res.data;
+          this.allLength = res.data.length;
+          this.pagingList = this.allBudgets;
+
+          this.allLength = this.pagingList.length;
           if (this.allLength % this.budgetPerPage === 0) {
             this.pagingLength = parseInt(this.allLength / this.budgetPerPage);
           } else {
-            this.pagingLength = parseInt(this.allLength / this.budgetPerPage) + 1;
+            this.pagingLength =
+              parseInt(this.allLength / this.budgetPerPage) + 1;
           }
           this.pages = 1;
           // console.log(res)
           // return res.data
-
         })
         .catch(err => {
-          console.log(err)
-        })
+          console.log(err);
+        });
+    },
+    showdetail(title) {
+      this.budgetDetail = []
+      this.showflag=true;
+      if(this.showflag==true){
+      console.log("--Parent (showdetail) : " + title);
+      this.budgetDetail.budget_title = title;
+      this.title = title
+      // this.budgetDetail =  
+      }
+      // this.showflag=false;      
     }
-
-
   },
   computed: {
     mountedBudget() {
-      console.log('mountedBudget')
-      this.allBudgets = this.getMyBudgets()
-      this.allLength = this.allBudgets.length
+      console.log("mountedBudget");
+      this.allBudgets = this.getMyBudgets();
+      this.allLength = this.allBudgets.length;
       return this.getMyBudgets();
     }
   },
   mounted() {
-    this.getMyBudgets();
-    // this.allBudgets = this.getMyBudgets()
-    // this.allPages = await FirebaseService.getMyBudgets();
-    // this.allLength = this.allPages.length;
-    // console.log("allLength : " + this.allPages.length);
-    // this.pagingList = await FirebaseService.getMyBudgetPaging(
-    //   (this.pages - 1) * 5,
-    //   (this.pages - 1) * 5 + 5,
-    //   this.allLength
-    // );
-    // console.log(this.pagingList);
-
-    // if (this.allLength % this.budgetPerPage === 0) {
-    //   this.pagingLength = parseInt(this.allLength / 5);
-    // } else {
-    //   this.pagingLength = parseInt(this.allLength / 5 + 1);
-    // }
-    // console.log("pagingLength : " + this.pagingLength);
-    //this.getMyBudgets()
+    this.getMyBudgets();   
   }
 };
 </script>
