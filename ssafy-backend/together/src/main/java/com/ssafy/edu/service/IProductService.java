bd15a902e3aa2d2d18821401dad97ef17058c607
subin <soobin1080@naver.com>
2020-02-26 21:47:05 +0900

    200226_soobin_product

--- ssafy-backend/together/src/main/java/com/ssafy/edu/service/IProductService.java
+++ ssafy-backend/together/src/main/java/com/ssafy/edu/service/IProductService.java
@@ -3,15 +3,12 @@ package com.ssafy.edu.service;
 import java.util.List;
 
 import com.ssafy.edu.model.Product;
+import com.ssafy.edu.model.ProductPage;
 
 public interface IProductService {
-	List<Product> getAllProduct();
-
-	List<Product> keywordSearchProduct(String keyword);
-
-	List<Product> categorySearchProduct(String category);
-
-	List<Product> categoryKeywordSearchProduct(String category, String keyword);
-
 	void insertProduct(Product product);
+
+	ProductPage getProductListWithPage(int nowPage, String category);
+
+	ProductPage getKeywordProductListWithPage(int page, String category, String keyword);
 }
--- ssafy-backend/together/src/main/java/com/ssafy/edu/service/ProductService.java
+++ ssafy-backend/together/src/main/java/com/ssafy/edu/service/ProductService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.ssafy.edu.dao.ProductDaoImpl;
 import com.ssafy.edu.model.Product;
+import com.ssafy.edu.model.ProductPage;
 
 @Service
 public class ProductService implements IProductService {
@@ -13,34 +14,42 @@ public class ProductService implements IProductService {
 	@Autowired
 	private ProductDaoImpl productdao;
 	
-	@Override
-	public List<Product> getAllProduct() {
-		// TODO Auto-generated method stub
-		return productdao.getAllProduct();
-	}
-
-	@Override
-	public List<Product> keywordSearchProduct(String keyword) {
-		// TODO Auto-generated method stub
-		return productdao.keywordSearchProduct(keyword);
-	}
-
-	@Override
-	public List<Product> categorySearchProduct(String category) {
-		// TODO Auto-generated method stub
-		return productdao.categorySearchProduct(category);
-	}
-
-	@Override
-	public List<Product> categoryKeywordSearchProduct(String category, String keyword) {
-		// TODO Auto-generated method stub
-		return productdao.categoryKeywordSearchProduct(category,keyword);
-	}
 
 	@Override
 	public void insertProduct(Product product) {
 		// TODO Auto-generated method stub
 		productdao.insertProduct(product);
+	}
+
+	@Override
+	public ProductPage getProductListWithPage(int nowPage, String category) {
+		//현재 페이지에 뿌려줄 상품 12개 가져오기
+		int totPage = (int) Math.ceil(productdao.cntTotFood(category) / 12.0);
+		System.out.println("totPage : " + totPage);
+		System.out.println("nowPage : " + nowPage);
+		
+		int startContent = (nowPage - 1) * 12;
+		System.out.println("startContent : " + startContent);
+
+		List<Product> products = productdao.getProductListWithPage(startContent,category);
+		
+		return new ProductPage(products, nowPage,1,totPage,totPage);
+	}
+
+
+	@Override
+	public ProductPage getKeywordProductListWithPage(int nowPage, String category, String keyword) {
+		//현재 페이지에 뿌려줄 상품 12개 가져오기
+		int totPage = (int) Math.ceil(productdao.cntTotFood(category) / 12.0);
+		System.out.println("totPage : " + totPage);
+		System.out.println("nowPage : " + nowPage);
+		
+		int startContent = (nowPage - 1) * 12;
+		System.out.println("startContent : " + startContent);
+
+		List<Product> products = productdao.getKeywordProductListWithPage(startContent,category,keyword);
+		
+		return new ProductPage(products, nowPage,1,totPage,totPage);
 	}
 	
 
